1c9c94d0e7711a7f375ec1559a501f24c872162f
oljas <olja.smirnov93@gmail.com>
2023-06-16 12:37:43 +0300

    CW#7 functional interface

---
+++ src/telran/numbers/Calculator.java
@@ -0,0 +1,8 @@
+package telran.numbers;
+
+public class Calculator {
+	static public double calculate(CalcData cd) {
+		//TODO
+		return 0;
+	}
+}
---
+++ src/telran/numbers/test/CalculatorTest.java
@@ -0,0 +1,42 @@
+package telran.numbers.test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.function.BinaryOperator;
+
+import org.junit.jupiter.api.Test;
+
+import telran.numbers.CalcData;
+import telran.numbers.Calculator;
+public class CalculatorTest {
+@Test
+void test() {
+	BinaryOperator<Double> bo = (a, b) -> a + b;
+	assertEquals(30, bo.apply(10.0, 20.0));
+//	BinaryOperator<Double> [] operators = new BinaryOperator[4];
+//	operators[0] = (a, b) -> a + b;
+//	operators[1] = (a, b) -> a - b;
+//	operators[2] = (a, b) -> a * b;
+//	operators[3] = (a, b) -> a / b;
+//	assertEquals(-10, operators[1].apply(10.0, 20.0));
+	DoubleBinaryOperator[] operators = {
+			(a, b) -> a + b,
+			(a, b) -> a - b,
+			(a, b) -> a * b,
+			(a, b) -> a / b
+	};
+	double[] res = {30, 10, 200, 2};
+	double op1 = 20;
+	double op2 = 10;
+	for(int i = 0; i < operators.length; i++) {
+		assertEquals(res[i],operators[i].apply(op1, op2));
+	}
+}
+@Test
+void calculateTest() {
+	assertEquals(20, Calculator.calculate(new CalcData(40, 20, '-')));
+}
+
+interface DoubleBinaryOperator extends BinaryOperator<Double>{
+	
+}
+}
