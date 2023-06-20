84a9cdaf6d80885c86afd1cec6c194c32bf2a15d
oljas <olja.smirnov93@gmail.com>
2023-06-20 18:20:27 +0300

    HW#7 calculator

--- src/telran/numbers/Calculator.java
+++ src/telran/numbers/Calculator.java
@@ -1,8 +1,37 @@
 package telran.numbers;
 
+import java.util.function.BinaryOperator;
+
 public class Calculator {
+	static DoubleBinaryOperator[] operators = {
+			(a, b) -> a + b,
+			(a, b) -> a - b,
+			(a, b) -> a * b,
+			(a, b) -> a / b
+	};
+	static char[] operations = {
+		'+','-','*','/'
+	};
+	
 	static public double calculate(CalcData cd) {
-		//TODO
-		return 0;
+		int index = findIndex(cd.operation);
+		
+		return operators[index].apply(cd.op1, cd.op2);
 	}
+
+	private static int findIndex(char operation) {
+		int res = -1;
+		int index = 0;
+		while(index < operations.length && res == -1) {
+			if(operations[index] == operation) {
+				res = index;
+			}
+			index++;
+		}
+
+		return res;
+	}
+}
+interface DoubleBinaryOperator extends BinaryOperator<Double> {
+	
 }
