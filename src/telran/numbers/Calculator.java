172967e7ec33ba114d396ae9446d239852fe4ca7
oljas <olja.smirnov93@gmail.com>
2023-06-21 20:18:47 +0300

    CW#8 Exceptions introduction

--- src/telran/numbers/Calculator.java
+++ src/telran/numbers/Calculator.java
@@ -7,15 +7,23 @@ public class Calculator {
 			(a, b) -> a + b,
 			(a, b) -> a - b,
 			(a, b) -> a * b,
-			(a, b) -> a / b
+			(a, b) -> {
+				if (b == 0) {
+					throw new ArithmeticException("division on 0");	
+				}
+				return a / b;
+			}
 	};
 	static char[] operations = {
-		'+','-','*','/'
+		'+','-','*','/', '%'
 	};
 	
 	static public double calculate(CalcData cd) {
 		int index = findIndex(cd.operation);
-		
+		if(index < 0) {
+			throw new UnsupportedOperationException("unsupported operation" + cd.operation);
+			
+		}
 		return operators[index].apply(cd.op1, cd.op2);
 	}
 
